d1b0585301975b1814039e782489724bd7c0ce20
kstamatis <kstamatis@ekt.gr>
2013-10-30 22:12:18 +0200

    Initial project

---
+++ DspaceRestAPIWrapper/DspaceRestAPIWrapper.h
@@ -0,0 +1,13 @@
+//
+//  DspaceRestAPIWrapper.h
+//  DspaceRestAPIWrapper
+//
+//  Created by Kostas Stamatis on 10/30/13.
+//  Copyright (c) 2013 kstamatis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DspaceRestAPIWrapper : NSObject
+
+@end
---
+++ DspaceRestAPIWrapper/DspaceRestAPIWrapper.m
@@ -0,0 +1,13 @@
+//
+//  DspaceRestAPIWrapper.m
+//  DspaceRestAPIWrapper
+//
+//  Created by Kostas Stamatis on 10/30/13.
+//  Copyright (c) 2013 kstamatis. All rights reserved.
+//
+
+#import "DspaceRestAPIWrapper.h"
+
+@implementation DspaceRestAPIWrapper
+
+@end
